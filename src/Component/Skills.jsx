ab8b7c2bbb6e7a2eb886a8977080aa95cc87fcc6
Pratik <pratikdjadhav16@gmail.com>
2024-10-06 21:30:16 +0530

    Update Skill Section and CV

--- src/Component/Skills.jsx
+++ src/Component/Skills.jsx
@@ -15,6 +15,12 @@ import lightGithub from "./Icons/icons8-github-100.png";
 import figma from "./Icons/figma-logo.png";
 import Bootstrap from "./Icons/bootstrap-icon.png";
 import materialUi from "./Icons/material-ui-logo.png";
+import Babel from "./Icons/babel_icon.png";
+import Npm from "./Icons/NPM_icon.png";
+import Webpack from "./Icons/webpack_icon.png";
+import Json from "./Icons/JSON_icon.png";
+import React_router from "./Icons/react-router_icon.png";
+import Axios from "./Icons/Azios_icon.png";
 import "./CSS/Skills.css";
 
 const Skills = ({ mode }) => {
@@ -26,42 +32,97 @@ const Skills = ({ mode }) => {
         </div>
         <div id="skillContainerBox">
           <div className="skillBox animationBox">
+            {/* JavaScript */}
             <div className="animationBox boxBgColor">
               <img className="icons" src={javaScript} alt="" />
               <span className="onHoverText techStyle">Javascript</span>
             </div>
+
+            {/* React */}
             <div className="animationBox boxBgColor">
               <img className="icons" src={react} alt="" />
               <span className="onHoverText techStyle">React</span>
             </div>
+
+            {/* React router */}
+            <div className="animationBox boxBgColor">
+              <img className="icons" src={React_router} alt="" />
+              <span className="onHoverText techStyle">React router</span>
+            </div>
+
+            {/* Redux toolkit */}
             <div className="animationBox boxBgColor">
               <img className="icons" src={redux} alt="" />
-              <span className="onHoverText techStyle">Redux Toolkit</span>
+              <span className="onHoverText techStyle">Redux toolkit</span>
             </div>
+
+            {/* Material UI */}
             <div className="animationBox boxBgColor">
               <img className="icons" src={materialUi} alt="" />
               <span className="onHoverText techStyle">Material UI</span>
             </div>
+
+            {/* Figma */}
             <div className="animationBox boxBgColor">
               <img className="icons" src={figma} alt="" />
               <span className="onHoverText techStyle">Figma</span>
             </div>
+
+            {/* CSS */}
             <div className="animationBox boxBgColor">
               <img className="icons" src={css} alt="" />
               <span className="onHoverText techStyle">CSS3</span>
             </div>
+
+            {/* Bootstrap */}
             <div className="animationBox boxBgColor">
               <img className="oracle icons" src={Bootstrap} alt="" />
               <span className="onHoverText techStyle">Bootstrap</span>
             </div>
+
+            {/* HTML */}
             <div className="animationBox boxBgColor">
               <img className="icons" src={html} alt="" />
               <span className="onHoverText techStyle">HTML5</span>
             </div>
+
+            {/* NPM */}
+            <div className="animationBox boxBgColor">
+              <img className="icons" src={Npm} alt="" />
+              <span className="onHoverText techStyle">NPM</span>
+            </div>
+
+            {/* Webpack */}
+            <div className="animationBox boxBgColor">
+              <img className="icons" src={Webpack} alt="" />
+              <span className="onHoverText techStyle">Webpack</span>
+            </div>
+
+            {/* Babel */}
+            <div className="animationBox boxBgColor">
+              <img className="icons" src={Babel} alt="" />
+              <span className="onHoverText techStyle">Babel</span>
+            </div>
+
+            {/* JSON */}
+            <div className="animationBox boxBgColor">
+              <img className="icons" src={Json} alt="" />
+              <span className="onHoverText techStyle">JSON</span>
+            </div>
+
+            {/* Axios */}
+            <div className="animationBox boxBgColor">
+              <img className="icons" src={Axios} alt="" />
+              <span className="onHoverText techStyle">AXIOS</span>
+            </div>
+
+            {/* Git */}
             <div className="animationBox boxBgColor">
               <img className="icons" src={git} alt="" />
               <span className="onHoverText techStyle">Git</span>
             </div>
+
+            {/* GitHub */}
             <div className="animationBox boxBgColor">
               {mode === "darkMode" ? (
                 <img className="github icons" src={darkGithub} alt="" />
@@ -70,22 +131,32 @@ const Skills = ({ mode }) => {
               )}
               <span className="onHoverText techStyle">GitHub</span>
             </div>
+
+            {/* VS Code */}
             <div className="animationBox boxBgColor">
               <img className="icons" src={vscode} alt="" />
               <span className="onHoverText techStyle">VS Code</span>
             </div>
+
+            {/* Node */}
             <div className="animationBox boxBgColor">
               <img className="icons" src={node} alt="" />
               <span className="onHoverText techStyle">Node</span>
             </div>
+
+            {/* Express */}
             <div className="animationBox boxBgColor">
               <img className="icons" src={express} alt="" />
               <span className="onHoverText techStyle">Express</span>
             </div>
+
+            {/* MongoDB */}
             <div className="animationBox boxBgColor">
               <img className="icons" src={mongodb} alt="" />
               <span className="onHoverText techStyle">MongoDB</span>
             </div>
+
+            {/* Oracle */}
             <div className="animationBox boxBgColor">
               <img className="oracle icons" src={oracle} alt="" />
               <span className="onHoverText techStyle">Oracle</span>

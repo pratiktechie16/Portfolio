c87222b58b7d33954766726a327dd7d7596f2e92
Pratik <pratikdjadhav16@gmail.com>
2025-06-16 22:24:28 +0530

    refactor: update links and restructure code

---
+++ src/Component/Constants/LinksConstant.js
@@ -0,0 +1,17 @@
+export const LINKS_CONSTANT = [
+    {
+        LINK: "https://www.linkedin.com/in/pratiktechie/",
+        ICON: "fa-linkedin",
+        TEXT: "Connect on Linkedin"
+    },
+    {
+        LINK: "https://github.com/pratiktechie16/",
+        ICON: "fa-github",
+        TEXT: "Check repose on Github"
+    },
+    {
+        LINK: "https://www.instagram.com/pratiktechie/",
+        ICON: "fa-instagram",
+        TEXT: "Follow on Instagram"
+    }
+]
--- src/Component/Links.jsx
+++ src/Component/Links.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./CSS/Links.css";
+import { LINKS_CONSTANT } from "./Constants/LinksConstant";
 
 const Links = () => {
   return (
@@ -9,77 +10,30 @@ const Links = () => {
           <h1 className="headingColor">Where to find me</h1>
         </div>
         <div id="linksContainerBox">
-          {/* Linkedin */}
-          <div className="linksDiv boxBgColor animationBox">
-            <i className="fa-brands fa-linkedin"></i>
-            <a
-              href="https://www.linkedin.com/in/pratikjdv16/"
-              className="headingColor"
-            >
-              Connect on Linkedin{" "}
-              <svg
-                width="0.625rem"
-                viewBox="0 0 10 10"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1 9L9 1M9 1H2.5M9 1V7.22222"
-                  stroke="currentColor"
-                  strokeWidth="1.25"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
-              </svg>
-            </a>
-          </div>
-
-          {/* Github */}
-          <div className="linksDiv boxBgColor animationBox">
-            <i className="fa-brands fa-github"></i>
-            <a href="https://github.com/Pratikjdv16" className="headingColor">
-              Check repose on Github{" "}
-              <svg
-                width="0.625rem"
-                viewBox="0 0 10 10"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1 9L9 1M9 1H2.5M9 1V7.22222"
-                  stroke="currentColor"
-                  strokeWidth="1.25"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
-              </svg>
-            </a>
-          </div>
-
-          {/* Instagram */}
-          <div className="linksDiv boxBgColor animationBox">
-            <i className="fa-brands fa-instagram"></i>
-            <a
-              href="https://www.instagram.com/pratiktechie/"
-              className="headingColor"
-            >
-              Follow on Instagram{" "}
-              <svg
-                width="0.625rem"
-                viewBox="0 0 10 10"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1 9L9 1M9 1H2.5M9 1V7.22222"
-                  stroke="currentColor"
-                  strokeWidth="1.25"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
-              </svg>
-            </a>
-          </div>
+          {LINKS_CONSTANT?.map((item, index) => {
+            return (
+              <div key={index} className="linksDiv boxBgColor animationBox">
+                <i className={`fa-brands ${item.ICON}`}></i>
+                <a href={item.LINK} className="headingColor">
+                  {item.TEXT}
+                  <svg
+                    width="0.625rem"
+                    viewBox="0 0 10 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M1 9L9 1M9 1H2.5M9 1V7.22222"
+                      stroke="currentColor"
+                      strokeWidth="1.25"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    ></path>
+                  </svg>
+                </a>
+              </div>
+            );
+          })}
         </div>
       </section>
     </>
